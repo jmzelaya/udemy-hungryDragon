881173b8b14146e665a4b9aeffd342354e7cf7a7
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2018-02-06 20:38:12 -0500

    added background and made it scroll

--- js/stateMain.js
+++ js/stateMain.js
@@ -5,14 +5,21 @@ var StateMain = {
     }
 
     game.load.spritesheet("dragon", "images/main/dragon.png", 120, 85, 4);
+    game.load.image("background", "images/main/background.png");
   },
 
   create: function () {
     //Dragon
     this.dragon = game.add.sprite(0, 0, "dragon");
     this.dragon.animations.add("fly", [0, 1, 2, 3], 12, true);
-
     this.dragon.animations.play("fly");
+
+    //background
+    this.background = game.add.tileSprite(0, game.height-480, game.width, 480, 'background');
+
+    this.dragon.bringToTop();
+
+    this.background.autoScroll(-100, 0);
 
     this.setListeners();
   },
