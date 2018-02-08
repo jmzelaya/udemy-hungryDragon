b7344e6c355b79307e5dc4b7c7bd4a4727f2bad6
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2018-02-07 20:04:48 -0500

    added physics to the dragon

--- js/stateMain.js
+++ js/stateMain.js
@@ -9,6 +9,11 @@ var StateMain = {
   },
 
   create: function () {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    this.top = 0;
+    this.bottom = game.height - 100;
+
     //Dragon
     this.dragon = game.add.sprite(0, 0, "dragon");
     this.dragon.animations.add("fly", [0, 1, 2, 3], 12, true);
@@ -17,7 +22,19 @@ var StateMain = {
     //background
     this.background = game.add.tileSprite(0, game.height-480, game.width, 480, 'background');
 
+    //IPAD FIX
+    if(screen.height>764){
+      this.background.y=game.world.centerY-
+      this.background.height/2;
+      this.top=this.background.y;
+    }
+
     this.dragon.bringToTop();
+    this.dragon.y=this.top;
+    //Dragon Physics
+    game.physics.enable(this.dragon,Phaser.Physics.ARCADE);
+
+    this.dragon.body.gravity.y = 500;
 
     this.background.autoScroll(-100, 0);
 
@@ -34,21 +51,28 @@ var StateMain = {
 
   wrongWay: function(){
     //Check to see if it detects the enterIncorrectOrientation
-    // console.log("wrongWay");
+    console.log("wrongWay");
     //When in wrongWay make the div #wrongWay display BLOCK
     document.getElementById("wrongWay").style.display="block";
   },
 
   rightWay:function(){
     //Check to see if it detects the leaveIncorrectOrientation
-    // console.log("rightWay");
+    console.log("rightWay");
     //When in the rightWay make the div #wrongWay display NONE
     document.getElementById("wrongWay").style.display="none";
   },
 
 
   update: function () {
+    if(this.dragon.y > this.bottom){
+      this.dragon.y = this.bottom;
+      this.dragon.body.gravity.y = 0;
+    }
 
+    else {
+      this.dragon.body.gravity.y = 500;
+    }
   },
 
 };
