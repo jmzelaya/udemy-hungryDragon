d3f1d10882128b358730bb11b57edaa398d4131c
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2018-02-11 22:39:18 -0500

    make game start at startTitle and added a start game screen with button which initializes the game

--- js/stateTitle.js
+++ js/stateTitle.js
@@ -1,10 +1,20 @@
 var StateTitle = {
   preload: function () {
+    game.load.spritesheet("buttons", "images/ui/buttons.png", 265,75);
 
   },
 
   create: function () {
+    this.buttonStart = game.add.button(
+      game.world.centerX, game.world.centerY + 100, "buttons",
+                 //over, normal, pressed
+                 //         |
+      this.startGame, this, 6, 7, 6);
+    this.buttonPlayAgain.anchor.set(0.5, 0.5);
+  },
 
+  startGame: function (){
+    game.state.start("StateMain");
   },
 
   update: function () {
